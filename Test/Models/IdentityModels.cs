7a936279a4b568cc1080ad55487ccbe5613c7bbb
Administrator <Administrator@PC011>
2019-04-03 12:49:08 +0700

    ghi code trong IndentityModels.cs

--- Test/Models/IdentityModels.cs
+++ Test/Models/IdentityModels.cs
@@ -15,6 +15,7 @@ namespace Test.Models
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
             // Add custom user claims here
             return userIdentity;
+            //ghi chu trong code
         }
     }
 
